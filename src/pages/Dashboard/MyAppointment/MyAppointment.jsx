9e416c18226a07317d7c9e9bdd4ce6c9410b43a9
MD Asraful Islam <asrafulislam.dev@gmail.com>
2024-08-06 22:55:10 +0600

    make a appointment table

--- src/pages/Dashboard/MyAppointment/MyAppointment.jsx
+++ src/pages/Dashboard/MyAppointment/MyAppointment.jsx
@@ -1,3 +1,13 @@
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+
 import { Button } from "@/components/ui/button";
 import { Calendar } from "@/components/ui/calendar";
 import { cn } from "@/lib/utils";
@@ -14,36 +24,61 @@ import { useState } from "react";
 const MyAppointment = () => {
   const [date, setDate] = useState(new Date());
   return (
-    <div className="container flex justify-between mx-auto">
-      <div className="max-w-[250px] h-screen hidden md:flex bg-slate-100 w-full p-4">
+    <div className="flex justify-between mx-auto">
+      <div className="max-w-[250px] h-screen hidden md:flex bg-slate-100 w-full p-8">
         <h1>My Appointment</h1>
       </div>
-      <div className="grid grid-cols-2 w-full justify-between px-7 py-12">
-        <h1 className="text-2xl">My Appointment</h1>
-        <div className="text-end">
-          <Popover>
-            <PopoverTrigger asChild>
-              <Button
-                variant={"outline"}
-                className={cn(
-                  "col-span-4 justify-start rounded- text-left font-normal",
-                  !date && "text-muted-foreground"
-                )}
-              >
-                <CalendarIcon className="mr-2 h-4 w-4" />
-                {date ? format(date, "PPP") : <span>Pick a date</span>}
-              </Button>
-            </PopoverTrigger>
-            <PopoverContent className="w-auto p-0 bg-white">
-              <Calendar
-                mode="single"
-                selected={date}
-                onSelect={setDate}
-                initialFocus
-              />
-            </PopoverContent>
-          </Popover>
+
+      <div className="grid w-full px-7 py-12">
+        <div className="grid grid-cols-2 w-full justify-between ">
+          <div>
+            <h1 className="text-2xl text-orange-600">My Appointment</h1>
+          </div>
+          <div className="text-end">
+            <Popover>
+              <PopoverTrigger asChild>
+                <Button
+                  variant={"outline"}
+                  className={cn(
+                    "col-span-4 justify-start rounded- text-left font-normal",
+                    !date && "text-muted-foreground"
+                  )}
+                >
+                  <CalendarIcon className="mr-2 h-4 w-4" />
+                  {date ? format(date, "PPP") : <span>Pick a date</span>}
+                </Button>
+              </PopoverTrigger>
+              <PopoverContent className="w-auto p-0 bg-white">
+                <Calendar
+                  mode="single"
+                  selected={date}
+                  onSelect={setDate}
+                  initialFocus
+                />
+              </PopoverContent>
+            </Popover>
+          </div>
         </div>
+
+        <Table>
+          <TableCaption>A list of your recent invoices.</TableCaption>
+          <TableHeader>
+            <TableRow>
+              <TableHead className="w-[100px]">Invoice</TableHead>
+              <TableHead>Status</TableHead>
+              <TableHead>Method</TableHead>
+              <TableHead className="text-right">Amount</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            <TableRow>
+              <TableCell className="font-medium">INV001</TableCell>
+              <TableCell>Paid</TableCell>
+              <TableCell>Credit Card</TableCell>
+              <TableCell className="text-right">$250.00</TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
       </div>
     </div>
   );
